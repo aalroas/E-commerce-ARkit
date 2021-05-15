3deb3a2c771787ba378e620f18ee3ee3d641262a
aalroas <mr.a.alroas@gmail.com>
2021-05-15 20:12:25 +0300

    Initial Commit

---
+++ gproject-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  gproject-ios
+//
+//  Created by Abdulsalam Alroas on 15/05/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
